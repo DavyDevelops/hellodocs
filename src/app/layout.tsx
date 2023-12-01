d9bfa4fa483da7cb41673be45f22ddec7881ee43
david nyong <davydevelops@gmail.com>
2023-12-01 09:48:42 +0100

    Hello docs world

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import {cn} from '@/lib/utils';
+import './globals.css'
+import Navbar from '@/components/Navbar';
+
+const inter = Inter({ subsets: ['latin'] })
+
+
+export const metadata: Metadata = {
+  title: 'Hello Docs :)',
+  description: 'Chat with your PDF',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className='light'>
+      <body className={cn(
+        'min-h-screen font-sans antialiased grainy',
+        inter.className
+      )}>
+        <Navbar/>
+        {children}
+        </body>
+    </html>
+  )
+}
